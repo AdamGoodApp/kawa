2a39834ae207cf7b361ebbed133c7aee677f9537
Adam Rais <raisadamo@gmail.com>
2024-10-01 21:34:14 +0900

    Add hompage test

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from 'react';
-import {ScrollView, useColorScheme, View, Text, Button, ImageBackground} from 'react-native';
-import { NavigationProp } from '@react-navigation/native';
+import {ScrollView, useColorScheme, View, Text, ImageBackground, TouchableOpacity} from 'react-native';
 
 import { getMovies } from '../../lib/tmdb';
 
-function Home({ navigation }: { navigation: NavigationProp<any> }): React.JSX.Element {
+function Home(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   const [movies, setMovies] = useState<MovieResponse>();
 
@@ -17,25 +16,27 @@ function Home({ navigation }: { navigation: NavigationProp<any> }): React.JSX.El
     fetchMovies();
   }, []);
 
+  const movie = movies?.movie_results[0];
+
   return (
     <ScrollView contentInsetAdjustmentBehavior="automatic">
-      {movies ? (
+      {movie ? (
         <ImageBackground
-          source={{ uri: `https://image.tmdb.org/t/p/original/${movies.movie_results[0].backdrop_path}` }}
+          source={{ uri: `https://image.tmdb.org/t/p/original/${movie.backdrop_path}` }}
           className="w-full h-full"
           resizeMode="cover"
         >
           <View className="flex justify-center items-center w-full h-screen">
             <Text className={`${isDarkMode ? 'text-white' : 'text-black'} text-4xl`}>Home page</Text>
             <Text className={`${isDarkMode ? 'text-white' : 'text-black'} text-2xl`}>
-              {movies.movie_results[0].original_title}
+              {movie.original_title}
             </Text>
-            <Button
-              title="Go to Settings"
-              onPress={() =>
-                navigation.navigate('Settings')
-              }
-            />
+            <TouchableOpacity>
+              <View className="h-[52px] px-[25px] bg-white/95 rounded-[500px] justify-center items-center gap-1.5 flex-row">
+                <Text className="text-center text-black text-[22px] font-['SF Pro'] leading-normal">&</Text>
+                <Text className="text-center text-black text-[19px] font-['SF Pro'] leading-normal">Label</Text>
+              </View>
+            </TouchableOpacity>
           </View>
         </ImageBackground>
       ) : (
